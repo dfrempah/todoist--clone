d02d0ef99e10af0251aa71c5f219f40f56dc13a7
dfrempah <dougyboy14@gmail.com>
2019-09-15 15:53:09 +0000

    cred removal

--- firebasestuff.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig  = firebase.initializeApp({
-    apiKey: "AIzaSyDMasX9XHC3gy941Yt8aLox3XYh0UDztFM",
-    authDomain: "todoist-biom.firebaseapp.com",
-    databaseURL: "https://todoist-biom.firebaseio.com",
-    projectId: "todoist-biom",
-    storageBucket: "todoist-biom.appspot.com",
-    messagingSenderId: "343151355389",
-    appId: "1:343151355389:web:cb7bc66853807fb3896594"
-})
